c92b2c0b3b0f7eb6c4235852fea2bd2e2ddb594e
ulrik <ulrik@ulriks-MacBook-Pro.local>
2023-05-25 10:04:29 +0200

    Updated nextconfig: Added experimental serverActions and whitelisted image domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const { i18n } = require("./next-i18next.config");
 
-const nextConfig = { i18n };
+const nextConfig = {
+  i18n,
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    domains: ["openweathermap.org"],
+  },
+};
 
 module.exports = nextConfig;
